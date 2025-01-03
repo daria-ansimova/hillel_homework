044367c93c48772db7bedf4cb232571236dc07ad
Daria Ansimova <dasha@unix4all.com>
2025-01-03 23:01:01 +0200

    task_1 improvement

--- lesson_2/task_1.py
+++ lesson_2/task_1.py
@@ -1,7 +1,7 @@
+# Vol.1
 # added built-in function abs() to provide returning absolute values of numbers
 number = abs(int(input("Enter 4-digit integer number: ")))
-
-# optimal solution to overwriting the value of the variable
+# optimal solution is to overwrite the value of the variable
 div1, number = divmod(number, 1000)
 div2, number = divmod(number, 100)
 div3, number = divmod(number, 10)
@@ -10,14 +10,13 @@ print(div2)
 print(div3)
 print(number)
 
-# another solution with creating additional variables, which is not optimal
-# can be appropriate in case of additional conditions in task
+# Vol.1
 # number = abs(int(input("Enter 4-digit integer number: ")))
-# div1, mod1 = divmod(number, 1000)
-# div2, mod2 = divmod(mod1, 100)
-# div3, mod3 = divmod(mod2, 10)
-# div4 = mod3
-# print(div1)
-# print(div2)
-# print(div3)
-# print(div4)
+# digit_1 = number//1000
+# digit_2 = (number//100)%10
+# digit_3 = (number//10)%10
+# digit_4 = number%10
+# print(digit_1)
+# print(digit_2)
+# print(digit_3)
+# print(digit_4)
