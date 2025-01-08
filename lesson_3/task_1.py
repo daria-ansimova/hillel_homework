77629fac41b61f4e000734d1f1d8ae03e4d841a7
Daria Ansimova <dasha@unix4all.com>
2025-01-08 21:34:43 +0200

    Addits to the Lesson 3 task_1

--- lesson_3/task_1.py
+++ lesson_3/task_1.py
@@ -7,13 +7,13 @@ if user_activity == "+":
 elif user_activity == "-":
     result = user_number_1 - user_number_2
 elif user_activity == "*":
-    result = 0.0 if user_number_1 == 0 or user_number_2 == 0 else user_number_1 * user_number_2
+    result = user_number_1 * user_number_2
 elif user_activity == "/":
     if user_number_2 == 0:
         result = None;
         print("You can't divide by 0")
     else:
-        result = 0.0 if user_number_1 == 0 else user_number_1 / user_number_2
+        result = user_number_1 / user_number_2
 else:
     result = None
     print("Error: something went wrong")
