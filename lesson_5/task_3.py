b1c6a179ef9e6da242b460722c8d84230aa0a465
Daria Ansimova <dasha@unix4all.com>
2025-01-19 00:12:05 +0200

    Lesson5_task1_task3

--- lesson_5/task_3.py
+++ lesson_5/task_3.py
@@ -1,9 +1,4 @@
 import string
 text_entrance = str(input("Enter your words combination or text: "))
 hashtag = '#' + text_entrance.title().translate(text_entrance.maketrans('', '', string.punctuation + ' '))
-
-if len(hashtag) > 140:
-    result = hashtag[:140]
-else:
-    result = hashtag
-print(result)
+print(hashtag[:140])
