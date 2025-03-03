da07a01333e2364a8c499f460aa39d1c44054c34
Daria Ansimova <dasha@unix4all.com>
2025-03-03 18:57:10 +0200

    Lesson_12_task2 + Lesson 13

--- lesson_12/task_2.py
+++ lesson_12/task_2.py
@@ -56,7 +56,7 @@ assert isinstance(cart.user, User) is True, 'Екземпляр класу User'
 assert cart.get_total() == 60, "Всього 60"
 assert cart.get_total() == 60, 'Повинно залишатися 60!'
 cart.add_item(apple, 10)
-# print(cart)
+print(cart)
 """
 User: Ivan Ivanov
 Items:
